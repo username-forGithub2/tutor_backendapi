044e417d12f2068a20f5c41e420c309c1c7028c7
usernameBakhtiyor <harhil46@gmail.com>
2023-05-17 20:01:04 +0500

    Add

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tutors-api.netlify.app/'
+    origins 'https://tutors-api.netlify.app'
     resource(
      "*",
       headers: :any,
